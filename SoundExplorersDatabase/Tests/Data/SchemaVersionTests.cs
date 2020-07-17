9d2d92947d76f7ea615be988d13e1a58be04bd91
Simon O'Rorke <simon.ororke@outlook.com>
2020-07-17 13:29:51 +1200

    Unit tests working in Rider

--- SoundExplorersDatabase/Tests/Data/SchemaVersionTests.cs
+++ SoundExplorersDatabase/Tests/Data/SchemaVersionTests.cs
@@ -1,6 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System.IO;
+using NUnit.Framework;
 using SoundExplorersDatabase.Data;
-using System.IO;
 
 namespace SoundExplorersDatabase.Tests.Data {
   [TestFixture]
