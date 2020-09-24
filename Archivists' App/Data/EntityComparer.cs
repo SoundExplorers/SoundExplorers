58eb6b3653ca03620b417ee8b61f1c809fb71036
Simon O'Rorke <simon.ororke@outlook.com>
2020-09-25 09:59:16 +1200

    Simplify EntityComparer

--- Archivists' App/Data/EntityComparer.cs
+++ Archivists' App/Data/EntityComparer.cs
@@ -3,10 +3,7 @@
 namespace SoundExplorers.Data {
   public class EntityComparer<TEntity> : Comparer<TEntity> where TEntity : IEntity {
     public override int Compare(TEntity x, TEntity y) {
-      if (x?.Key < y?.Key) {
-        return -1;
-      }
-      return x?.Key == y?.Key ? 0 : 1;
+      return new KeyComparer().Compare(x?.Key, y?.Key);
     }
   }
 }
