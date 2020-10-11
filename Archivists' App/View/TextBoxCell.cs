c2d2b8efa8f4e622dd455306e19342bf111f8cac
Simon O'Rorke <simon.ororke@outlook.com>
2020-10-11 18:37:41 +1300

    Select edited cell contents on F2.

--- Archivists' App/View/TextBoxCell.cs
+++ Archivists' App/View/TextBoxCell.cs
@@ -6,7 +6,10 @@ namespace SoundExplorers.View {
   ///   TextBox cell of a DataGridView.
   /// </summary>
   /// <remarks>
-  ///   The cell's entire contents are selected on beginning edit or Ctrl+A.
+  ///   The cell's entire contents are selected on beginning edit or Ctrl+A or F2.
+  ///   For unknown reason, the initial text selection works if the cell edit was started
+  ///   with a mouse click but not if started with the F2 key.
+  ///   As a partial workaround, the select all is triggered if F2 is pressed again.
   /// </remarks>
   public class TextBoxCell : DataGridViewTextBoxCell {
     public override Type EditType => typeof(DataGridViewTextBoxEditingControl);
@@ -15,15 +18,27 @@ namespace SoundExplorers.View {
       int rowIndex,
       object initialFormattedValue,
       DataGridViewCellStyle dataGridViewCellStyle) {
+      // Debug.WriteLine(
+      //   $"{nameof(TextBoxCell)}.{nameof(InitializeEditingControl)}");
       base.InitializeEditingControl(rowIndex, initialFormattedValue,
         dataGridViewCellStyle);
       var textBox = (TextBox)DataGridView.EditingControl;
-      textBox.SelectAll();
       textBox.KeyUp += TextBoxOnKeyUp;
+      textBox.SelectAll();
+      // I tried a timer to fix the failure of the initial SelectAll when
+      // the cell edit was started with the F2 key.  It does not work.
+      // var timer = new Timer();
+      // timer.Tick += (sender, args) => {
+      //   timer.Stop();
+      //   textBox.SelectAll();
+      // };
     }
 
     private static void TextBoxOnKeyUp(object sender, KeyEventArgs e) {
-      if (e.Modifiers == Keys.Control && e.KeyCode == Keys.A) {
+      // Debug.WriteLine(
+      //   $"{nameof(TextBoxCell)}.{nameof(TextBoxOnKeyUp)}: KeyCode = {e.KeyCode}");
+      if (e.Modifiers == Keys.Control && e.KeyCode == Keys.A ||
+          e.Modifiers == Keys.None && e.KeyCode == Keys.F2) {
         ((TextBox)sender).SelectAll();
       }
     }
