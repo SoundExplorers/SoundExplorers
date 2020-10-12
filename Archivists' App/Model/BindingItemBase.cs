b07e50c7a970155d35235adcbcca2298e6a0fc5a
Simon O'Rorke <simon.ororke@outlook.com>
2020-10-12 17:16:09 +1300

    On update error, allow error value to be modified.

--- Archivists' App/Model/BindingItemBase.cs
+++ Archivists' App/Model/BindingItemBase.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using JetBrains.Annotations;
 
@@ -10,27 +11,31 @@ namespace SoundExplorers.Model {
   ///   (with ListChangedEventArgs.ListChangedType == ListChangedType.ItemChanged)
   ///   when there there is a change to the value of a property of a new or existing item.
   ///   Each derived class should also be marked with
-  ///   <see cref="NoReorderAttribute"/> to ensure that the order in which properties
+  ///   <see cref="NoReorderAttribute" /> to ensure that the order in which properties
   ///   are declared in the derived class will not change
   ///   when the Resharper code cleanup is run.
   /// </summary>
   /// <remarks>
-  ///   The <see cref="NoReorderAttribute"/> is important because the property order
+  ///   The <see cref="NoReorderAttribute" /> is important because the property order
   ///   determines the order in which the corresponding columns will be displayed
   ///   on the grid.
-  ///   Derived classes that were developed without <see cref="NoReorderAttribute"/>
+  ///   Derived classes that were developed without <see cref="NoReorderAttribute" />
   ///   did not actually have their property order changed by code cleanup,
   ///   presumably because their structure is so simple and, in particular,
   ///   they don't implement interfaces, which affect the code cleanup order.
   ///   Other derived classes are expected to be just as simple.
-  ///   So the <see cref="NoReorderAttribute"/> is a safety feature.
+  ///   So the <see cref="NoReorderAttribute" /> is a safety feature.
   /// </remarks>
   public abstract class BindingItemBase : INotifyPropertyChanged {
     public event PropertyChangedEventHandler PropertyChanged;
 
     [CanBeNull]
     internal object GetPropertyValue([NotNull] string propertyName) {
-    } 
+      return (
+        from property in GetType().GetProperties()
+        where property.Name == propertyName
+        select property).First().GetValue(this);
+    }
 
     [NotifyPropertyChangedInvocator]
     protected void OnPropertyChanged(
