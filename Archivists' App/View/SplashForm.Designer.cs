632a842884e0088b5a3ca0d34b52d59831a2cdda
Simon O'Rorke <simon.ororke@outlook.com>
2020-12-21 12:05:50 +1300

    .Net 5 Migration: Removed splash window picture to avoid it momentarily being shown as a hole in the window.

--- Archivists' App/View/SplashForm.Designer.cs
+++ Archivists' App/View/SplashForm.Designer.cs
@@ -24,68 +24,56 @@ namespace SoundExplorers.View {
     /// </summary>
     private void InitializeComponent() {
       System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SplashForm));
-      this.productNameLabel = new System.Windows.Forms.Label();
-      this.pictureBox1 = new System.Windows.Forms.PictureBox();
+      this.ProductNameTextBox = new System.Windows.Forms.TextBox();
       this.SplashPanel.SuspendLayout();
-      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
       this.SuspendLayout();
       // 
       // SplashPanel
       // 
       this.SplashPanel.BackColor = System.Drawing.Color.Firebrick;
-      this.SplashPanel.Controls.Add(this.productNameLabel);
-      this.SplashPanel.Controls.Add(this.pictureBox1);
-      this.SplashPanel.Size = new System.Drawing.Size(566, 360);
-      this.SplashPanel.Controls.SetChildIndex(this.pictureBox1, 0);
-      this.SplashPanel.Controls.SetChildIndex(this.productNameLabel, 0);
+      this.SplashPanel.Controls.Add(this.ProductNameTextBox);
+      this.SplashPanel.Size = new System.Drawing.Size(484, 254);
       this.SplashPanel.Controls.SetChildIndex(this.StatusLabel, 0);
+      this.SplashPanel.Controls.SetChildIndex(this.ProductNameTextBox, 0);
       // 
       // StatusLabel
       // 
-      this.StatusLabel.Location = new System.Drawing.Point(0, 298);
-      this.StatusLabel.Size = new System.Drawing.Size(566, 62);
+      this.StatusLabel.Location = new System.Drawing.Point(0, 192);
+      this.StatusLabel.Size = new System.Drawing.Size(484, 62);
       this.StatusLabel.Text = "Status Label";
       // 
-      // productNameLabel
+      // ProductNameTextBox
       // 
-      this.productNameLabel.Dock = System.Windows.Forms.DockStyle.Top;
-      this.productNameLabel.Font = new System.Drawing.Font("Old English Text MT", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-      this.productNameLabel.ForeColor = System.Drawing.Color.Gold;
-      this.productNameLabel.Location = new System.Drawing.Point(0, 0);
-      this.productNameLabel.Name = "productNameLabel";
-      this.productNameLabel.Size = new System.Drawing.Size(566, 89);
-      this.productNameLabel.TabIndex = 4;
-      this.productNameLabel.Text = "Sound Explorers Audio Archive";
-      this.productNameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-      // 
-      // pictureBox1
-      // 
-      this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-      this.pictureBox1.ImageLocation = "";
-      this.pictureBox1.Location = new System.Drawing.Point(185, 92);
-      this.pictureBox1.Name = "pictureBox1";
-      this.pictureBox1.Size = new System.Drawing.Size(200, 200);
-      this.pictureBox1.TabIndex = 3;
-      this.pictureBox1.TabStop = false;
+      this.ProductNameTextBox.BackColor = System.Drawing.Color.Firebrick;
+      this.ProductNameTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+      this.ProductNameTextBox.Font = new System.Drawing.Font("Old English Text MT", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+      this.ProductNameTextBox.ForeColor = System.Drawing.Color.Gold;
+      this.ProductNameTextBox.Location = new System.Drawing.Point(40, 0);
+      this.ProductNameTextBox.Multiline = true;
+      this.ProductNameTextBox.Name = "ProductNameTextBox";
+      this.ProductNameTextBox.ReadOnly = true;
+      this.ProductNameTextBox.Size = new System.Drawing.Size(397, 109);
+      this.ProductNameTextBox.TabIndex = 5;
+      this.ProductNameTextBox.TabStop = false;
+      this.ProductNameTextBox.Text = "Sound Explorers Audio Archive";
+      this.ProductNameTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
       // 
       // SplashForm
       // 
       this.AutoScaleBaseSize = new System.Drawing.Size(11, 25);
       this.BackColor = System.Drawing.Color.Gold;
-      this.ClientSize = new System.Drawing.Size(582, 376);
+      this.ClientSize = new System.Drawing.Size(500, 270);
       this.ForeColor = System.Drawing.SystemColors.ControlText;
       this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
       this.Name = "SplashForm";
-      this.Text = "Sound Explorers Audio Archive";
+      this.Text = " ";
       this.SplashPanel.ResumeLayout(false);
-      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+      this.SplashPanel.PerformLayout();
       this.ResumeLayout(false);
 
     }
-    
-    private System.Windows.Forms.PictureBox pictureBox1;
-    private System.Windows.Forms.Label productNameLabel;
-
     #endregion
+
+    private System.Windows.Forms.TextBox ProductNameTextBox;
   }
 }
