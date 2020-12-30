e0df0ecec2375310df93f0263bd2909ad29f9c13
Simon O'Rorke <simon.ororke@outlook.com>
2020-12-30 15:43:23 +1300

    Test data generator: improved Set.Act generation

--- Archivists' App/Tests/TestDatabaseGenerator.cs
+++ Archivists' App/Tests/TestDatabaseGenerator.cs
@@ -12,7 +12,7 @@ namespace SoundExplorers.Tests {
     /// <summary>
     ///   1 to enable generate
     /// </summary>
-    private static int DoIt => 0;
+    private static int DoIt => 1;
 
     /// <summary>
     ///   If the main test database folder already exists,
@@ -40,61 +40,44 @@ namespace SoundExplorers.Tests {
       data.AddNewslettersPersisted(64, session);
       data.AddSeriesPersisted(8, session);
       data.AddEventsPersisted(18, session);
-      for (var i = 0; i < 5; i++) {
+      for (int i = 0; i < 5; i++) {
         data.Events[i].EventType = data.EventTypes[i];
       }
-      for (var i = 5; i < 10; i++) {
+      for (int i = 5; i < 10; i++) {
         data.Events[i].EventType = data.EventTypes[i - 5];
       }
-      for (var i = 10; i < 15; i++) {
+      for (int i = 10; i < 15; i++) {
         data.Events[i].EventType = data.EventTypes[i - 10];
       }
-      for (var i = 0; i < 16; i++) {
+      for (int i = 0; i < 16; i++) {
         data.Events[i].Newsletter = data.Newsletters[i];
       }
-      for (var i = 0; i < 8; i++) {
+      for (int i = 0; i < 8; i++) {
         data.Events[i].Location = data.Locations[i];
         data.Events[i].Series = data.Series[i];
       }
-      for (var i = 8; i < 16; i++) {
+      for (int i = 8; i < 16; i++) {
         data.Events[i].Location = data.Locations[i - 8];
         data.Events[i].Series = data.Series[i - 8];
       }
-      for (var i = 0; i < 5; i++) {
+      for (int i = 0; i < 5; i++) {
         data.AddSetsPersisted(i + 1, session, data.Events[i], data.Genres[i]);
       }
-      for (var i = 5; i < 10; i++) {
+      for (int i = 5; i < 10; i++) {
         data.AddSetsPersisted(i - 4, session, data.Events[i], data.Genres[i]);
       }
-      for (var i = 10; i < 15; i++) {
+      for (int i = 10; i < 15; i++) {
         data.AddSetsPersisted(i - 9, session, data.Events[i], data.Genres[i - 10]);
       }
-      for (var i = 15; i < 18; i++) {
+      for (int i = 15; i < 18; i++) {
         data.AddSetsPersisted(i - 9, session, data.Events[i]);
       }
-      for (var i = 0; i < 10; i++) {
-        data.Sets[i].Act = data.Acts[i];
-      }
-      for (var i = 10; i < 15; i++) {
-        data.Sets[i].Act = data.Acts[i - 10];
-      }
-      for (var i = 15; i < 20; i++) {
-        data.Sets[i].Act = data.Acts[i - 15];
-      }
-      for (var i = 20; i < 25; i++) {
-        data.Sets[i].Act = data.Acts[i - 20];
-      }
-      for (var i = 25; i < 30; i++) {
-        data.Sets[i].Act = data.Acts[i - 25];
-      }
-      for (var i = 30; i < 35; i++) {
-        data.Sets[i].Act = data.Acts[i - 30];
-      }
-      for (var i = 35; i < 40; i++) {
-        data.Sets[i].Act = data.Acts[i - 35];
-      }
-      for (var i = 40; i < 45; i++) {
-        data.Sets[i].Act = data.Acts[i - 40];
+      for (int i = 0; i < data.Events.Count; i++) {
+        var @event = data.Events[i];
+        bool isEventIndexEven = i % 2 == 0;
+        for (int j = 0; j < @event.Sets.Count; j++) {
+          @event.Sets[j].Act = isEventIndexEven ? data.Acts[j] : data.Acts[j + 2];
+        }
       }
       session.Commit();
     }
