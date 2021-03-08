dcd6de0a4eb39d02633fb5a1c920c004971d82d8
Simon O'Rorke <simon.ororke@outlook.com>
2021-03-09 08:28:23 +1300

    Key parameters of KeyComparer.Compare are now non-nullable.

--- Archivists' App/Data/KeyComparer.cs
+++ Archivists' App/Data/KeyComparer.cs
@@ -1,24 +1,12 @@
-﻿using System;
-using VelocityDb.Collection.Comparer;
+﻿using VelocityDb.Collection.Comparer;
 
 namespace SoundExplorers.Data {
   public class KeyComparer : VelocityDbComparer<Key> {
-    public override int Compare(Key? key1, Key? key2) {
-      ValidateKeys(key1, key2);
+    public override int Compare(Key key1, Key key2) {
       if (key1 < key2) {
         return -1;
       }
       return key1 == key2 ? 0 : 1;
-    }
-
-    private static void ValidateKeys(Key? key1, Key? key2) {
-      if (key1 == null || key2 == null) {
-        string key1String = key1 == null ? "null" : $"'{key1}'";
-        string key2String = key2 == null ? "null" : $"'{key2}'";
-        throw new InvalidOperationException(
-          "A null Key argument was passed to KeyComparer.Compare: " +
-          $"key1 = {key1String}; key2 = {key2String}.");
-      }
     }
   }
 }
--- Archivists' App/Tests/Data/KeyComparerTests.cs
+++ Archivists' App/Tests/Data/KeyComparerTests.cs
@@ -13,7 +13,7 @@ namespace SoundExplorers.Tests.Data {
     private KeyComparer KeyComparer { get; set; } = null!;
 
     [Test]
-    public void Equal() {
+    public void Equals() {
       var key1 = new Key("A", null);
       var key2 = new Key("A", null);
       Assert.AreEqual(0, KeyComparer.Compare(key1, key2));
@@ -31,19 +31,6 @@ namespace SoundExplorers.Tests.Data {
       var key1 = new Key("A", null);
       var key2 = new Key("B", null);
       Assert.AreEqual(-1, KeyComparer.Compare(key1, key2));
-    }
-
-    [Test]
-    public void NullKey() {
-      var key1 = new Key("A", null);
-      int unused;
-      var exception =
-        Assert.Catch<InvalidOperationException>(
-          () => unused = KeyComparer.Compare(key1, null),
-          "Null key disallowed");
-      Assert.AreEqual(
-        "A null Key argument was passed to KeyComparer.Compare: key1 = 'A'; key2 = null.", 
-        exception.Message, "Error message");
     }
   }
 }
