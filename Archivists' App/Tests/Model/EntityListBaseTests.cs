69a09759a51a1a1072d29024bbb7a9dada7c85a1
Simon O'Rorke <simon.ororke@outlook.com>
2021-03-10 00:17:05 +1300

    Brought back and fixed EntityListBaseTests

---
+++ Archivists' App/Tests/Model/EntityListBaseTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+using SoundExplorers.Model;
+using SoundExplorers.Tests.Data;
+
+namespace SoundExplorers.Tests.Model {
+  [TestFixture]
+  public class EntityListBaseTests {
+    [SetUp]
+    public void Setup() {
+      Session = new TestSession();
+    }
+
+    [TearDown]
+    public void TearDown() {
+      Session.DeleteDatabaseFolderIfExists();
+    }
+
+    private class DudErrorThrowerList : NamedEntityList<ErrorThrowingEventType> {
+    }
+
+    private TestSession Session { get; set; } = null!;
+
+    [Test]
+    public void RethrowDudError() {
+      var list = new DudErrorThrowerList {Session = Session};
+      list.Populate(); // Creates an empty BindingList
+      var bindingList = list.BindingList;
+      var item1 = bindingList.AddNew();
+      list.OnRowEnter(0);
+      item1.Name = "Dudley";
+      Assert.Throws<InvalidOperationException>(() => list.OnRowValidated(0));
+    }
+  }
+}
